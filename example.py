b3feeb7acb564372c1ad867514b144a147f002a2
toinsson <antoine.loriette@gmail.com>
2016-11-14 18:15:32 +0000

    split context and add class for device, add constants file parsed during setup from rs.h file

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import pyrealsense as pyrs
 import matplotlib.pyplot as plt
-pyrs.start()
-dm = pyrs.get_depth()
+dev = pyrs.start()
+dm = dev.get_depth()
 plt.imshow(dm); plt.show()
 pyrs.stop()
--- setup.py
+++ setup.py
@@ -1,6 +1,68 @@
 from setuptools import setup
 from setuptools import find_packages
 
+import pycparser
+import numpy as np
+
+
+## do platform dependent
+rs_h_filename = "/usr/local/include/librealsense/rs.h"
+
+
+## extract RS_API in #define
+rs_api = []
+with open(rs_h_filename, encoding='latin') as rs_h_file:
+    for l in rs_h_file.readlines():
+        if "RS_API" in l:
+            rs_api.append(" = ".join(l.split()[1:]))
+        if len(rs_api) == 3: break
+
+    versions = [int(x.split()[2]) for x in rs_api]
+    rs_api.append("RS_API_VERSION = " + str((np.array(versions) * [10000, 100, 1]).sum()))
+
+    with open("./pyrealsense/constants.py", "w") as constants:
+        constants.write("\n".join(rs_api) + "\n\n")
+
+
+## parse the header
+ast = pycparser.parse_file(rs_h_filename, use_cpp=True)
+
+
+## extract the enums from parsed header
+enums_to_map = ['rs_capabilities', 'rs_stream', 'rs_format']
+
+
+def get_enumlist(obj):
+    for cn, c in obj.children():
+        if type(c) is pycparser.c_ast.EnumeratorList:
+            return c
+        else:
+            return get_enumlist(c)
+
+
+def write_enumlist(f, obj, name):
+    classname = "class " + name + ":"
+    enumerates = []
+
+    for i, (cn, c) in enumerate(obj.children()):
+        enumerates.append("    "+c.name + " = " + str(i))
+
+    f.write("\n".join([classname] + enumerates) + "\n\n")
+
+
+with open("./pyrealsense/constants.py", "a") as constants:
+
+    for c in ast.ext:
+        if c.name in enums_to_map:
+            e = get_enumlist(c)
+            write_enumlist(constants, e, c.name)
+
+
+## find name of enum we are interested in
+# 'rs_capabilities', 'rs_stream', rs_format'
+# ast.ext[0].show(attrnames=True, nodenames=True, showcoord=True)
+# dump to file and install locally
+# use NodeVisitor for that matter and copy Node.show structure
 
 setup ( name = 'pyrealsense',
         version = '1.0',
