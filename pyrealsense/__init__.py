a794ee4cda79d97ac5b9e5af2b3168dee1a0ddf5
antoine.loriette <antoine.loriette@gmail.com>
2016-12-11 12:42:56 +0000

    to wrap file

--- pyrealsense/__init__.py
+++ pyrealsense/__init__.py
@@ -8,6 +8,8 @@ import ctypes
 from pyrealsense import constants as cnst
 from pyrealsense.constants import rs_stream, rs_format
 from pyrealsense.stream import ColourStream, DepthStream, PointStream, CADStream
+from pyrealsense.to_wrap import rs_error, rs_intrinsics
+
 
 # hack to load "extension" module
 import os
@@ -17,26 +19,6 @@ rsutilwrapper = ctypes.CDLL(os.path.join(_DIRNAME,'rsutilwrapper.so'))
 ## import C lib
 lrs = ctypes.CDLL('librealsense.so')
 
-
-class rs_intrinsics(ctypes.Structure):
-    _fields_ = [
-        ("width", ctypes.c_int),
-        ("height", ctypes.c_int),
-        ("ppx", ctypes.c_float),
-        ("ppy", ctypes.c_float),
-        ("fx", ctypes.c_float),
-        ("fy", ctypes.c_float),
-        ("model", ctypes.c_int),        #rs_distortion
-        ("coeffs", ctypes.c_float*5),
-    ]
-
-
-## ERROR handling
-class rs_error(ctypes.Structure):
-    _fields_ = [("message", ctypes.c_char_p),
-                ("function", ctypes.POINTER(ctypes.c_char)),
-                ("args", ctypes.c_char_p),
-                ]
 
 def pp(fun, *args):
     """Wrapper for printing char pointer from ctypes."""
---
+++ pyrealsense/to_wrap.py
@@ -0,0 +1,21 @@
+import ctypes
+
+class rs_intrinsics(ctypes.Structure):
+    _fields_ = [
+        ("width", ctypes.c_int),
+        ("height", ctypes.c_int),
+        ("ppx", ctypes.c_float),
+        ("ppy", ctypes.c_float),
+        ("fx", ctypes.c_float),
+        ("fy", ctypes.c_float),
+        ("model", ctypes.c_int),        #rs_distortion
+        ("coeffs", ctypes.c_float*5),
+    ]
+
+
+## ERROR handling
+class rs_error(ctypes.Structure):
+    _fields_ = [("message", ctypes.c_char_p),
+                ("function", ctypes.POINTER(ctypes.c_char)),
+                ("args", ctypes.c_char_p),
+                ]
