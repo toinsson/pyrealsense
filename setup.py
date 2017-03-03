1fba89f2f568ed589f5ae768b5e23eed1b197a5d
Steven A. Cholewiak <scholewiak@gmail.com>
2017-03-03 13:32:28 -0800

    Cleaning up for more consistency with https://packaging.python.org/distributing/

--- setup.py
+++ setup.py
@@ -1,35 +1,11 @@
 from setuptools import setup, Extension
 from setuptools import find_packages
+from os import path
 
 import pycparser
 import numpy as np
-
 import io
 
-## do platform dependent
-rs_h_filename = "/usr/local/include/librealsense/rs.h"
-
-
-## extract RS_API in #define
-rs_api = []
-with io.open(rs_h_filename, encoding='latin') as rs_h_file:
-    for l in rs_h_file.readlines():
-        if "RS_API" in l:
-            rs_api.append(" = ".join(l.split()[1:]))
-        if len(rs_api) == 3: break
-    versions = [int(x.split()[2]) for x in rs_api]
-    rs_api.append("RS_API_VERSION = " + str((np.array(versions) * [10000, 100, 1]).sum()))
-    with open("./pyrealsense/constants.py", "w") as constants:
-        constants.write("\n".join(rs_api) + "\n\n")
-
-
-## parse the header
-ast = pycparser.parse_file(rs_h_filename, use_cpp=True)
-
-
-## extract the enums from parsed header
-enums_to_map = ['rs_capabilities', 'rs_stream', 'rs_format', 'rs_distortion', 'rs_ivcam_preset',
-    'rs_option']
 
 def get_enumlist(obj):
     for cn, c in obj.children():
@@ -38,6 +14,7 @@ def get_enumlist(obj):
         else:
             return get_enumlist(c)
 
+
 def write_enumlist(f, obj, name):
     classname = "class " + name + "(object):"
     enumerates = []
@@ -45,25 +22,61 @@ def write_enumlist(f, obj, name):
         enumerates.append("    "+c.name + " = " + str(i))
     f.write("\n".join([classname] + enumerates) + "\n\n")
 
+
+# do platform dependent
+rs_h_filename = "/usr/local/include/librealsense/rs.h"
+
+# extract RS_API in #define
+rs_api = []
+with io.open(rs_h_filename, encoding='latin') as rs_h_file:
+    for l in rs_h_file.readlines():
+        if "RS_API" in l:
+            rs_api.append(" = ".join(l.split()[1:]))
+        if len(rs_api) == 3:
+            break
+    versions = [int(x.split()[2]) for x in rs_api]
+    rs_api.append("RS_API_VERSION = " + str((np.array(versions) * [10000, 100, 1]).sum()))
+    with open("./pyrealsense/constants.py", "w") as constants:
+        constants.write("\n".join(rs_api) + "\n\n")
+
+ast = pycparser.parse_file(rs_h_filename, use_cpp=True)
 with open("./pyrealsense/constants.py", "a") as constants:
     for c in ast.ext:
-        if c.name in enums_to_map:
+        if c.name in ['rs_capabilities',
+                      'rs_stream',
+                      'rs_format',
+                      'rs_distortion',
+                      'rs_ivcam_preset',
+                      'rs_option']:
             e = get_enumlist(c)
             write_enumlist(constants, e, c.name)
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
-## setup
-module = [
-    Extension( 'pyrealsense.rsutilwrapper',
-               sources = ['pyrealsense/rsutilwrapper.c'],
-               libraries = ['realsense'],
-               include_dirs = [np.get_include(),'/usr/local/include/librealsense'],
-               library_dirs = ['/usr/local/lib'],
-            )
-    ]
+module = [Extension('pyrealsense.rsutilwrapper',
+                    sources=['pyrealsense/rsutilwrapper.c'],
+                    libraries=['realsense'],
+                    include_dirs=[np.get_include(), '/usr/local/include/librealsense'],
+                    library_dirs=['/usr/local/lib'], )]
 
-setup ( name = 'pyrealsense',
-        version = '1.3',
-        ext_modules = module,
-        packages = find_packages(),
-        )
+setup(name='pyrealsense',
+      version='1.3',
+
+      description='Simple ctypes extension to the librealsense library for Linux and Mac OS',
+      long_description=long_description,
+      url='https://github.com/toinsson/pyrealsense',
+      license='Apache',
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: System :: Hardware',
+      ],
+      keywords='realsense',
+
+      packages=find_packages(),
+      ext_modules=module,
+      install_requires=['numpy', 'pycparser'],)
