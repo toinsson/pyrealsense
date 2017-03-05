77048cef58480ac3ebccf1a9001990d02c4de670
antoine.loriette <antoine.loriette@gmail.com>
2017-03-05 16:30:40 +0000

    fix python 2.7

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, Extension
 from setuptools import find_packages
 from os import path
+import io
 
 import numpy as np
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 module = [Extension('pyrealsense.rsutilwrapper',
