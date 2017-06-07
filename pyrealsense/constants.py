6abfca60361b91fc1effd248163bbc55cd25c483
Zac Stewart <zgstewart@gmail.com>
2017-06-07 11:59:04 -0400

    Allow loading librealsense header via PYRS_INCLUDES on linux

--- pyrealsense/constants.py
+++ pyrealsense/constants.py
@@ -12,16 +12,19 @@ from os import environ, path
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
     rs_h_filename = './rs.h'
+
 ## for windows, use PYRS_INCLUDE
 elif sys.platform == 'win32':
-
     if 'PYRS_INCLUDES' not in environ:
         raise Exception('PYRS_INCLUDES must be set to the location of the librealsense headers!')
     else:
         rs_h_filename = path.join(environ['PYRS_INCLUDES'], 'rs.h')
 ## on other platforms, fall back on default location
 else:
-    rs_h_filename = '/usr/local/include/librealsense/rs.h'
+    if 'PYRS_INCLUDES' in environ:
+        rs_h_filename = path.join(environ['PYRS_INCLUDES'], 'rs.h')
+    else:
+        rs_h_filename = '/usr/local/include/librealsense/rs.h'
 
 ## if not found, exit
 if not path.exists(rs_h_filename):
