e674fe96a95422167a270eb76f5d3ea14b03122f
Pablo Prietz <pablo@prietz.org>
2017-08-18 10:07:17 +0200

    Revert show_cv2 example

--- examples/show_cv2.py
+++ examples/show_cv2.py
@@ -8,31 +8,8 @@ import pyrealsense as pyrs
 from pyrealsense.constants import rs_option
 
 
-color_stream = pyrs.stream.ColorStream(color_format='bgr')
-depth_stream = pyrs.stream.DepthStream()
-
-
-def convert_z16_to_bgr(frame):
-    hist = np.histogram(frame, bins=0x10000)[0]
-    hist = np.cumsum(hist)
-    hist -= hist[0]
-    rgb_frame = np.empty(frame.shape[:2] + (3,), dtype=np.uint8)
-
-    zeros = frame == 0
-    non_zeros = frame != 0
-
-    f = hist[frame[non_zeros]] * 255 / hist[0xFFFF]
-    rgb_frame[non_zeros, 0] = 255 - f
-    rgb_frame[non_zeros, 1] = 0
-    rgb_frame[non_zeros, 2] = f
-    rgb_frame[zeros, 0] = 20
-    rgb_frame[zeros, 1] = 5
-    rgb_frame[zeros, 2] = 0
-    return rgb_frame
-
-
 with pyrs.Service() as serv:
-    with serv.Device(streams=(color_stream, depth_stream)) as dev:
+    with serv.Device() as dev:
 
         dev.apply_ivcam_preset(0)
 
@@ -42,7 +19,6 @@ with pyrs.Service() as serv:
             dev.set_device_options(*zip(*custom_options))
         except pyrs.RealsenseError:
             pass  # options are not available on all devices
-
 
         cnt = 0
         last = time.time()
@@ -60,10 +36,10 @@ with pyrs.Service() as serv:
                 last = now
 
             dev.wait_for_frames()
-            c = dev.color  # set to bgr
-            d = dev.depth  # * dev.depth_scale * 1000
-            # d = cv2.applyColorMap(d.astype(np.uint8), cv2.COLORMAP_RAINBOW)
-            d = convert_z16_to_bgr(d)
+            c = dev.color
+            c = cv2.cvtColor(c, cv2.COLOR_RGB2BGR)
+            d = dev.depth * dev.depth_scale * 1000
+            d = cv2.applyColorMap(d.astype(np.uint8), cv2.COLORMAP_RAINBOW)
 
             cd = np.concatenate((c, d), axis=1)
 
